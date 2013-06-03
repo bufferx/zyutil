36611de2b6835c9ab1eefac01d2b93d557126418
bufferx (Zhang ZY) <idup2x@gmail.com>
2013-06-03 18:59:14 +0800

    add get_ip_address function

--- common.py
+++ common.py
@@ -176,6 +176,24 @@ class CommonUtil(object):
 
         return the_key
 
+def get_ip_address(ifname):
+    '''
+    Reference: http://www.pythonclub.org/python-network-application/get-ip-address
+    '''
+    import socket
+    import fcntl
+    import struct
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15])
+        )[20:24])
+    except IOError, e:
+        return None
+
 def main():
     pass
 
